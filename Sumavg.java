7b8bb845bd6d9e4629759c92c5cb0bd2db436aa2
varshithreddy45 <varsh6111@gmail.com>
2025-07-26 11:09:47 +0530

    Add files via upload

---
+++ Sumavg.java
@@ -0,0 +1,12 @@
+class Sumavg()
+{
+public static void main(String args[])
+{
+int sum=0,avg;
+for(i=1;i<=10;i++)
+{
+sum=sum+i;
+}
+System.out.println("sum:"+sum);
+avg=sum//10;
+System.out.println("avg:"+avg);
